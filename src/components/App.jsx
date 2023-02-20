b506183ec8dd11732cbc927106477d042ef4873b
Anastasia Koptilova <koptilova.nastya@gmail.com>
2023-02-20 21:27:18 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import css from './App.module.css';
+import { GlobalStyle } from './GlobalStyle';
+import { HomePage } from 'pages/HomePage';
+import { MoviesPage } from 'pages/MoviesPage/MoviesPage';
+import { MovieDetailsPage } from 'pages/MovieDetailsPage/MovieDetailsPage';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movies" element={<MoviesPage />} />
+      <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+        <Route path="cast" element={<Cast />} />
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/services/API.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const KEY = '24bdb15dbc109e85ecedc1baa3876895';
+
+export async function getTrending() {
+  const { data } = await axios.get(
+    `${BASE_URL}trending/all/day?api_key=${KEY}`
+  );
+  return data;
+}
+
+export async function getSearchMovies(query, page) {
+  const { data } = await axios.get(
+    `${BASE_URL}search/movie?query=${query}&api_key=${KEY}&language=en-US&page=${page}&include_adult=false`
+  );
+  return data;
+}
+
+export async function getMovieDetails(id) {
+  const { data } = await axios.get(
+    `${BASE_URL}movie/${id}?api_key=${KEY}&language=en-US`
+  );
+  return data;
+}
+
+export async function getMovieCredits(id) {
+  const { data } = await axios.get(
+    `${BASE_URL}movie/${id}/credits?api_key=${KEY}&language=en-US`
+  );
+  return data;
+}
+
+export async function getMovieReviews(id, page) {
+  const { data } = await axios.get(
+    `${BASE_URL}movie/${id}/reviews?api_key=${KEY}&language=en-US&page=${page}`
+  );
+  return data;
+}
